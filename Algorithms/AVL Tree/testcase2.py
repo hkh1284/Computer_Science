b08953f8fe8e6c1c10da0c48fa2a84898f1da41a
HeoGyeongHwan <68696099+hkh1284@users.noreply.github.com>
2023-06-02 00:22:42 +0900

    Create testcase2.py

---
+++ Algorithms/AVL Tree/testcase2.py
@@ -0,0 +1,26 @@
+# 두번째 테스트
+tree2 = AVLTree()
+tree2.insert(21)
+tree2.insert(6)
+tree2.insert(26)
+tree2.insert(18)
+tree2.insert(3)
+
+print("AVL 트리의 중위 순회 결과:")
+tree2.inorder()
+
+print("\nAVL 트리에 노드 14 삽입 수행")
+tree2.insert(14)
+
+print("노드 14 삽입 후, 중위 순회 결과:")
+tree2.inorder()
+
+
+print("\n노드 21을 삭제한 후 AVL 트리의 중위 순회 결과:")
+tree2.delete(21)
+tree2.inorder()
+
+print("\n노드 21의 존재 여부:")
+tree2.search(21)
+print("\n노드 26의 존재 여부:")
+tree2.search(14)
